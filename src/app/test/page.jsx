70afd096340faad166d723114936dfad8a904b9a
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2025-11-11 16:24:30 +0500

    added console

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 
 export default function TestPage() {
   const router = useRouter();
+  console.log("test page added");
   return (
     <div>
       <h1>Test Page</h1>
