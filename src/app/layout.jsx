24f901562aa1b71d6e843c1e5f95944e0c4e9fc2
AhsanHafeez-dev <ahsanhafeez724@gmail.com>
2025-11-11 18:25:55 +0500

    got

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,31 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import AuthProvider from "@/context/auth-context";
-import InstructorContext from "@/context/instructor-context";
-import StudentProvider from "@/context/student-context";
-import ProtectedLayout from "./ProtectedLayout"; // move to a separate file
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Edu Platform",
-  description: "Dynamic Next.js app",
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}>
         <AuthProvider>
           <InstructorContext>
             <StudentProvider>
